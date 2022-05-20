7713fcc04b607212c796930b4240b952b4b888b3
Le-nem <96234818+Le-nem@users.noreply.github.com>
2022-05-20 11:35:38 +0200

    Maj 20/05

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Personne;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,7 +17,7 @@ class PersonneController extends AbstractController
     {
         $repository = $doctrine->getRepository(Personne::class);
         $personnes = $repository->findAll();
-        return $this->render('personne/index.html.twig', ['personnes' => $personnes]);
+        return $this->render('personne/index.html.twig', ['personnes' => $personnes, 'isPaginated' => true]);
     }
 
     #[Route('/{id<\d+>}', name: 'detail_personne')]
@@ -28,15 +29,24 @@ class PersonneController extends AbstractController
             $this->addFlash('error', "La personne n'existe pas");
             return $this->redirectToRoute('index_personne');
         }
-        return $this->render('personne/detail.html.twig', ['personne' => $personne]);
+        return $this->render('personne/detail.html.twig', ['personne' => $personne, 'isPaginated' => true]);
     }
 
     #[Route('/all/{page?1}/{nbr?10}/{sortby?id}/{sort?asc}', name: 'all_personne')]
     public function all(ManagerRegistry $doctrine, $nbr, $page, $sortby, $sort): Response
     {
         $repository = $doctrine->getRepository(Personne::class);
+        $nbPersonne = $repository->count([]);
+        $nbrPage = ceil($nbPersonne / $nbr);
         $personnes = $repository->findBy([], [$sortby => $sort], $nbr, ($page - 1) * $nbr);
-        return $this->render('personne/index.html.twig', ['personnes' => $personnes]);
+
+        return $this->render('personne/index.html.twig', [
+            'personnes' => $personnes,
+            'isPaginated' => true,
+            'nbrPage' => $nbrPage,
+            'page' => $page,
+            'nbr' => $nbr
+        ]);
     }
 
     #[Route('/add', name: 'add_personne')]
@@ -54,5 +64,41 @@ class PersonneController extends AbstractController
         return $this->render('personne/detail.html.twig', [
             'personne' => $personne,
         ]);
+    }
+    #[Route('/delete/{id}', name: 'delete_personne')]
+    public function deletePersonne(Personne $personne = null, ManagerRegistry $doctrine): RedirectResponse
+    {
+        if ($personne) {
+            $manager = $doctrine->getManager();
+            $manager->remove($personne);
+            $manager->flush();
+            $this->addFlash('success', 'Supression ok');
+        } else {
+            $this->addFlash('error', 'Personne innexistante');
+        }
+        return $this->redirectToRoute('all_personne');
+    }
+    #[Route('/update/{id}/{name}/{firstname}/{age}', name: 'update_personne')]
+    public function updatePersonne(Personne $personne = null, $name, $firstname, $age, ManagerRegistry $doctrine): RedirectResponse
+    {
+        if ($personne) {
+            $manager = $doctrine->getManager();
+            $personne->setName($name);
+            $personne->setFirstname($firstname);
+            $personne->setAge($age);
+            $manager->persist($personne);
+            $manager->flush();
+            $this->addFlash('success', 'Edition Ok');
+        } else {
+            $this->addFlash('error', 'Edition pas OK');
+        }
+        return $this->redirectToRoute('all_personne');
+    }
+    #[Route('/stats/age/{ageMin}/{ageMax}', name: 'stat_personne')]
+    public function statPersonne(ManagerRegistry $doctrine, $ageMin, $ageMax): Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $stats = $repository->statsPersonnesByAgeInterval($ageMin, $ageMax);
+        return $this->render('personne/stats.html.twig', ['stats' => $stats[0], 'ageMin' => $ageMin, 'ageMax' => $ageMax]);
     }
 }
--- src/Repository/PersonneRepository.php
+++ src/Repository/PersonneRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Personne;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -39,28 +40,29 @@ class PersonneRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Personne[] Returns an array of Personne objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function statsPersonnesByAgeInterval($ageMin, $ageMax)
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->select('avg(p.age) as ageMoyen, count(p.id) as nbrPersonne');
+        $this->addIntervalAge($qb, $ageMin, $ageMax);
+        return $qb->getQuery()->getScalarResult();
+    }
 
-//    public function findOneBySomeField($value): ?Personne
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    private function addIntervalAge(QueryBuilder $qb, $ageMin, $ageMax)
+    {
+        $qb->andWhere('p.age >= :ageMin and p.age <= :ageMax')
+            ->setParameters(['ageMin' => $ageMin, 'ageMax' => $ageMax]);
+    }
+
+
+
+    //    public function findOneBySomeField($value): ?Personne
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
